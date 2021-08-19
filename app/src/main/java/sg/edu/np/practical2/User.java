b942322e4216822ddde84baa72121d488a3e83c8
Jeng Seong Ian Wee <s10203038@connect.np.edu.sg>
2021-08-20 00:03:58 +0800

    only committed 1 file by accident

--- app/src/main/java/sg/edu/np/practical2/User.java
+++ app/src/main/java/sg/edu/np/practical2/User.java
@@ -1,15 +1,8 @@
 package sg.edu.np.practical2;
 
 public class User {
-    String name;
-    String description;
-    int Id;
-    Boolean followed;
-
-    public User(String name, String description, int id, Boolean followed) {
-        this.name = name;
-        this.description = description;
-        Id = id;
-        this.followed = followed;
-    }
+    public String name;
+    public String description;
+    public int id;
+    public boolean followed;
 }
